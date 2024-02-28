42beca8c196965b56a18f020e2af83edf353ef15
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-28 20:20:21 +0900

    mongodb 임시 비활성화 + reactor tools 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+//	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	// reactor tool
+	implementation("io.projectreactor:reactor-tools")
+	
 	runtimeOnly("com.mysql:mysql-connector-j")
 	runtimeOnly("io.asyncer:r2dbc-mysql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
