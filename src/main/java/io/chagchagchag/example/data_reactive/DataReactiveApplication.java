fc801616171cc105f997013e94cc51f589abb8f7
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-28 20:17:02 +0900

    first status

---
+++ src/main/java/io/chagchagchag/example/data_reactive/DataReactiveApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.data_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/chagchagchag/example/data_reactive/DataReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.data_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
