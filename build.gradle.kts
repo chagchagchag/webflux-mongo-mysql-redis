3f5871d461dbb4371909218fd3a635bded49e8d3
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-28 21:03:29 +0900

    security 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.security:spring-security-core")
 
 	// reactor tool
 	implementation("io.projectreactor:reactor-tools")
