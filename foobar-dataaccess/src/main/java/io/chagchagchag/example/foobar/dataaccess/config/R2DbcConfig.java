e3df7ae9ae39624ebeeee4fd07b4a5abca7175e8
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-29 10:54:32 +0900

    멀티모듈 작업

--- foobar-dataaccess/src/main/java/io/chagchagchag/example/foobar/dataaccess/config/R2DbcConfig.java
+++ foobar-dataaccess/src/main/java/io/chagchagchag/example/foobar/dataaccess/config/R2DbcConfig.java
@@ -1,4 +1,4 @@
-package io.chagchagchag.example.foobar.user.config;
+package io.chagchagchag.example.foobar.dataaccess.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
