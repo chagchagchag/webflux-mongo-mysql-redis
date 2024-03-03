83d3699ef4bfc6f784aaa0eaa8ff62be802286bb
chagchagchag <chagchagchag.dev@gmail.com>
2024-03-03 16:46:52 +0900

    동기 비동기 예제

--- demo-concurrent/src/main/java/io/chagchagchag/example/foobar/concurrent/sync_async/simple/SimpleAsyncExample.java
+++ demo-concurrent/src/main/java/io/chagchagchag/example/foobar/concurrent/sync_async/simple/SimpleAsyncExample.java
@@ -1,5 +1,27 @@
 package io.chagchagchag.example.foobar.concurrent.sync_async.simple;
 
-public class SimpleAsyncExample {
+import java.time.LocalTime;
+import java.util.function.Consumer;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+public class SimpleAsyncExample {
+  public static void main(String [] args){
+    log.info("(start) main function " + LocalTime.now());
+    execLongDelayJob(i -> {
+      var result = i + i;
+      log.info("result == {}", result);
+    });
+    log.info("(end) main function " + LocalTime.now());
+  }
+
+  public static void execLongDelayJob(Consumer<Integer> consumer){
+    final long current = System.currentTimeMillis();
+    while(true){
+      final long spent = System.currentTimeMillis() - current;
+      if(spent > 1000) break;
+    }
+
+    consumer.accept(500);
+  }
 }
--- demo-concurrent/src/main/java/io/chagchagchag/example/foobar/concurrent/sync_async/simple/SimpleSyncExample.java
+++ demo-concurrent/src/main/java/io/chagchagchag/example/foobar/concurrent/sync_async/simple/SimpleSyncExample.java
@@ -1,5 +1,24 @@
 package io.chagchagchag.example.foobar.concurrent.sync_async.simple;
 
-public class SimpleSyncExample {
+import java.time.LocalTime;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+public class SimpleSyncExample {
+  public static void main(String [] args){
+    log.info("(start) main function " + LocalTime.now());
+    var result = getLongDelayJob();
+    var increased = result + 1;
+    assert increased == 501;
+    log.info("(end) main function " + LocalTime.now());
+  }
+
+  public static int getLongDelayJob(){
+    long current = System.currentTimeMillis();
+    while(true){
+      long timeSpent = System.currentTimeMillis() - current;
+      if(timeSpent > 1000) break;
+    }
+    return 500;
+  }
 }
