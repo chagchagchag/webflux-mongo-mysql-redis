9dfa2f97ea6c4b11a8c404cb0253f164a17e6d9a
chagchagchag <chagchagchag.dev@gmail.com>
2024-03-05 12:26:16 +0900

    SecurityConfig

--- foobar-user/src/main/java/io/chagchagchag/example/foobar/user/config/security/SecurityConfig.java
+++ foobar-user/src/main/java/io/chagchagchag/example/foobar/user/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -14,6 +15,7 @@ import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 @Configuration
 public class SecurityConfig {
 
